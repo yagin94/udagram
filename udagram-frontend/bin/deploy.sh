32d245d5b026c6c1b707e5ba509abfbd6cb0276b
Nguyen Duy Hien <nguyenduy.hien@petronas.com>
2022-10-04 14:47:24 +0700

    udagram

---
+++ udagram-frontend/bin/deploy.sh
@@ -0,0 +1,2 @@
+aws s3 cp --recursive --acl public-read ./www s3://bucket-udagram1715-0387-3812/
+aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://bucket-udagram1715-0387-3812/
